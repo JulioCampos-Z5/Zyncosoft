d90380712bad74a22742c2128abcb8695fb9c723
ju4d <rabarex15@gmail.com>
2026-07-22 10:52:43 -0600

    feat: 404

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,20 @@ import Features from './components/Features'
 import Process from './components/Process'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import NotFound from './components/NotFound'
 import { ScrollProgressBar, SideDots } from './components/ScrollUI'
 import WhatsappFab from './components/WhatsappFab'
 import { useSmoothScroll } from './hooks/useSmoothScroll'
 
+const isKnownRoute = ['', '/', '/index.html'].includes(
+  window.location.pathname,
+)
+
 export default function App() {
   useSmoothScroll()
+
+  if (!isKnownRoute) return <NotFound />
+
   return (
     <>
       <ScrollProgressBar />
